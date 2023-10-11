b83a0d2de79fd1613037f30fb99c6cf58e4bd39f
48484848484848 <145894541+48484848484848@users.noreply.github.com>
2023-10-11 19:20:46 +0900

    Update ast.c

--- function_proof/ast.c
+++ function_proof/ast.c
@@ -115,9 +115,43 @@ void print_returntype_def(json_value node, int *func_count){
 }
 
 
-void print_func_name(json_value node, int *func_count){
-    char *name = json_get_string(node, "name");
-    printf("%d번째 함수 이름 : %s\n",*func_count,name);
+void print_func_name(json_value node, int *func_count) {
+    json_value decl_name = json_get(node, "name");
+    if (decl_name != NULL) {
+        char *name = json_get_string(decl_name, "name");
+        if (name != NULL) {
+            printf("%d번째 함수 이름 : %s\n", *func_count, name);
+        } else {
+            printf("%d번째 함수 이름을 찾을 수 없습니다.\n", *func_count);
+        }
+    }
+}
+
+void print_func_params(json_value node, int *func_count) {
+    json_value decl_params = json_get(node, "param_decls");
+
+    if (decl_params != NULL) {
+        printf("%d번째 함수 파라미터:\n", *func_count);
+
+        for (int i = 0; i < json_len(decl_params); i++) {
+            json_value param = json_get(decl_params, i);
+
+            json_value param_type = json_get(param, "type");
+            json_value param_name = json_get(param, "name");
+
+            if (param_type != NULL && param_name != NULL) {
+                char *type_str = json_get_string(param_type, "names");
+                char *name_str = json_get_string(param_name, "name");
+                if (type_str != NULL && name_str != NULL) {
+                    printf("타입: %s, 변수명: %s\n", type_str, name_str);
+                } else {
+                    printf("파라미터 정보를 찾을 수 없습니다.\n");
+                }
+            } 
+        }
+    } else {
+        printf("%d번째 함수 파라미터 정보를 찾을 수 없습니다.\n", *func_count);
+    }
 }
 
 
